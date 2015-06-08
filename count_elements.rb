25754b4c275b4ac02cad6439525608fca1580fe2
AnnaErshova <anna.ershova@gmail.com>
2015-06-08 09:26:26 -0400

    all done

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
+# Count the elements in an Array by returning a hash whose 
+# keys are the elements and values the number of times they occur. Your goal is to create a histogram.
+# count_elements should return a hash with the count of each element in the larger array
+# count_elements should return a hash with the count of each element in the simple array
+# count_elements should return a hash with the count of each element in the medium array
+
 def count_elements(array)
-  # code goes here
+  histogram = Hash.new(0)
+  array.collect {|word| histogram[word] +=1} 
+  histogram
 end
- 
+
+# general way of making a hash from an array is 
+# hash = Hash[array.collect { |v| [v, v] }], so we are just counting it here
